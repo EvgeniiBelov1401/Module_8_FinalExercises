b304d5ac2707ed95cee09d3c8cbd06b40797bd83
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-06 17:11:00 +0300

    Добавил Task_1

---
+++ Module_8_FinalExercises/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        //Напишите программу, которая чистит нужную нам папку от файлов  и папок, 
+        //которые не использовались более 30 минут
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
