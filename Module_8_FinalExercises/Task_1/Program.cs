57521d6af66fcd4fd54bde8642646a57b110a35a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-06 17:51:05 +0300

    сделал программу с проверками

--- Module_8_FinalExercises/Task_1/Program.cs
+++ Module_8_FinalExercises/Task_1/Program.cs
@@ -6,7 +6,35 @@
         //которые не использовались более 30 минут
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int minutes = 30;
+            string dirPath = @"D:\Programming\Skillfactory\C#_projects\Module_8_FinalExercises\Module_8_FinalExercises\Task_1\FolderForTask1\TestFolder";
+            DirectoryInfo directory = new DirectoryInfo(dirPath);
+
+            if (directory.Exists)
+            {
+                if (DateTime.Now-directory.LastAccessTime>TimeSpan.FromMinutes(minutes))
+                {
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    Console.WriteLine("Все файлы удалены");
+                    foreach (DirectoryInfo dir in directory.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+                    Console.WriteLine("Все папки удалены");
+                }
+                else
+                {
+                    Console.WriteLine($"Время последнего доступа меньше {minutes} минут");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+            
         }
     }
 }
