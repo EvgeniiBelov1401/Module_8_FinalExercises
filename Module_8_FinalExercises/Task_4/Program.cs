ab4858ba99cc68223cff8366cc5e61dfbc446c71
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-09 20:41:26 +0300

    Добавил чтение из файла

--- Module_8_FinalExercises/Task_4/Program.cs
+++ Module_8_FinalExercises/Task_4/Program.cs
@@ -1,21 +1,53 @@
-﻿using Task_4.Modules;
+﻿using System.Reflection.PortableExecutable;
+using System.Text;
+using Task_4.Modules;
 
 namespace Task_4
 {
     internal class Program
     {
+        /*
+         * Написать программу-загрузчик данных из бинарного формата в текст.
+
+         На вход программа получает бинарный файл, предположительно, это база данных студентов.
+
+            Свойства сущности Student:
+
+            Имя — Name (string);
+            Группа — Group (string);
+            Дата рождения — DateOfBirth (DateTime).
+            Средний балл — (decimal).
+            Ваша программа должна:
+
+            Cчитать данные о студентах из файла;
+            Создать на рабочем столе директорию Students.
+            Внутри раскидать всех студентов из файла по группам (каждая группа-отдельный текстовый файл), в файле группы студенты перечислены построчно в формате "Имя, дата рождения, средний балл".
+        */
+
         static void Main(string[] args)
         {
             string filePath = @"D:\Programming\Skillfactory\C#_projects\Module_8_FinalExercises\Module_8_FinalExercises\Task_4\DataFolder\students.dat";
-            List<Students> allStudentsList = FillStudenstList();
-
+            
 
             Console.WriteLine("Для записи всех студентов нажмите 'ENTER'");
             Console.ReadLine();
-            WriteStudentsInFile(allStudentsList,filePath);
+            WriteStudentsInFile(FillStudenstList(), filePath);
+
             Console.Clear();
             Console.WriteLine("Данные успешно записаны");
+            Console.ReadLine();
+            Console.Clear();
 
+            Console.WriteLine("Чтобы посмотреть всех студентов нажмите 'ENTER'");
+            Console.ReadLine();
+            Console.Clear();
+
+            List<Students> studentsToRead = ShowAllStudentsList(filePath);
+            
+            foreach(Students student in studentsToRead)
+            {
+                Console.WriteLine($"Имя: {student.Name}\tГруппа: {student.Group}\t\tДата рождения: {student.DateOfBirth.ToString("dd.MM.yyyy")}\t Средний балл: {student.AverageScore}");
+            }
         }
 
         static List<Students> FillStudenstList()
@@ -37,7 +69,9 @@ namespace Task_4
 
         static void WriteStudentsInFile(List<Students>students,string filePath)
         {
-           BinaryWriter bw=new BinaryWriter(new FileStream(filePath, FileMode.Create));
+            using FileStream fs = new FileStream(filePath, FileMode.Create);
+            using BinaryWriter bw = new BinaryWriter(fs);
+
             foreach (Students student in students)
             {
                 bw.Write(student.Name);
@@ -45,7 +79,46 @@ namespace Task_4
                 bw.Write(student.DateOfBirth.ToBinary());
                 bw.Write(student.AverageScore);
             }
+            bw.Flush();
             bw.Close();
+            fs.Close();
+
+        }
+
+        static List<Students> ShowAllStudentsList(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                List<Students> result = new();
+                using FileStream fs = new FileStream(filePath, FileMode.Open);
+                using StreamReader sr = new StreamReader(fs);
+
+                Console.WriteLine(sr.ReadToEnd());
+
+                fs.Position = 0;
+
+                BinaryReader br = new BinaryReader(fs);
+
+                while (fs.Position < fs.Length)
+                {
+                    Students student = new Students();
+                    student.Name = br.ReadString();
+                    student.Group = br.ReadString();
+                    long dt = br.ReadInt64();
+                    student.DateOfBirth = DateTime.FromBinary(dt);
+                    student.AverageScore = br.ReadDecimal();
+
+                    result.Add(student);
+                }
+
+                fs.Close();
+                return result;
+            }
+            else
+            {
+                return new List<Students>();
+            }
+            
         }
     }
 }
